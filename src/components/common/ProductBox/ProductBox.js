9f3bda2653a87842869be6e73c894e4eb4c8d711
Mariusz Politowicz <politowiczmar@gmail.com>
2023-04-16 09:41:29 +0200

    Change product compare value on click

--- src/components/common/ProductBox/ProductBox.js
+++ src/components/common/ProductBox/ProductBox.js
@@ -10,58 +10,77 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 import { faStar as farStar, faHeart } from '@fortawesome/free-regular-svg-icons';
 import Button from '../Button/Button';
+import { getComparedProducts, toggleCompare } from '../../../redux/productsRedux';
+import { useDispatch, useSelector } from 'react-redux';
 
-const ProductBox = ({ name, price, promo, stars, oldPrice, favorite, compare }) => (
-  <div className={styles.root}>
-    <div className={styles.photo}>
-      <img
-        className={styles.image}
-        alt={name}
-        src={`${process.env.PUBLIC_URL}/images/products/${name}.jpg`}
-      />
-      {promo && <div className={styles.sale}>{promo}</div>}
-      <div className={styles.buttons}>
-        <Button variant='small'>Quick View</Button>
-        <Button variant='small'>
-          <FontAwesomeIcon icon={faShoppingBasket}></FontAwesomeIcon> ADD TO CART
-        </Button>
+const ProductBox = ({ id, name, price, promo, stars, oldPrice, favorite, compare }) => {
+  const dispatch = useDispatch();
+  const products = useSelector(getComparedProducts);
+
+  const handleToggleCompareProduct = e => {
+    e.preventDefault();
+    if (products.length < 4 || compare === true) {
+      dispatch(toggleCompare(id));
+    }
+  };
+
+  return (
+    <div className={styles.root}>
+      <div className={styles.photo}>
+        <img
+          className={styles.image}
+          alt={name}
+          src={`${process.env.PUBLIC_URL}/images/products/${name}.jpg`}
+        />
+        {promo && <div className={styles.sale}>{promo}</div>}
+        <div className={styles.buttons}>
+          <Button variant='small'>Quick View</Button>
+          <Button variant='small'>
+            <FontAwesomeIcon icon={faShoppingBasket}></FontAwesomeIcon> ADD TO CART
+          </Button>
+        </div>
+      </div>
+      <div className={styles.content}>
+        <h5>{name}</h5>
+        <div className={styles.stars}>
+          {[1, 2, 3, 4, 5].map(i => (
+            <a key={i} href='#'>
+              {i <= stars ? (
+                <FontAwesomeIcon icon={faStar}>{i} stars</FontAwesomeIcon>
+              ) : (
+                <FontAwesomeIcon icon={farStar}>{i} stars</FontAwesomeIcon>
+              )}
+            </a>
+          ))}
+        </div>
+      </div>
+      <div className={styles.line}></div>
+      <div className={styles.actions}>
+        <div className={styles.outlines}>
+          <Button className={favorite && styles.favorite} variant='outline'>
+            <FontAwesomeIcon icon={faHeart}>Favorite</FontAwesomeIcon>
+          </Button>
+          <Button
+            className={compare && styles.compare}
+            onClick={handleToggleCompareProduct}
+            variant='outline'
+          >
+            <FontAwesomeIcon icon={faExchangeAlt}>Add to compare</FontAwesomeIcon>
+          </Button>
+        </div>
+        <div className={styles.price}>
+          {oldPrice && <p>${oldPrice}</p>}
+          <Button noHover variant='small'>
+            $ {price}
+          </Button>
+        </div>
       </div>
     </div>
-    <div className={styles.content}>
-      <h5>{name}</h5>
-      <div className={styles.stars}>
-        {[1, 2, 3, 4, 5].map(i => (
-          <a key={i} href='#'>
-            {i <= stars ? (
-              <FontAwesomeIcon icon={faStar}>{i} stars</FontAwesomeIcon>
-            ) : (
-              <FontAwesomeIcon icon={farStar}>{i} stars</FontAwesomeIcon>
-            )}
-          </a>
-        ))}
-      </div>
-    </div>
-    <div className={styles.line}></div>
-    <div className={styles.actions}>
-      <div className={styles.outlines}>
-        <Button className={favorite && styles.favorite} variant='outline'>
-          <FontAwesomeIcon icon={faHeart}>Favorite</FontAwesomeIcon>
-        </Button>
-        <Button className={compare && styles.compare} variant='outline'>
-          <FontAwesomeIcon icon={faExchangeAlt}>Add to compare</FontAwesomeIcon>
-        </Button>
-      </div>
-      <div className={styles.price}>
-        {oldPrice && <p>${oldPrice}</p>}
-        <Button noHover variant='small'>
-          $ {price}
-        </Button>
-      </div>
-    </div>
-  </div>
-);
+  );
+};
 
 ProductBox.propTypes = {
+  id: PropTypes.string,
   children: PropTypes.node,
   name: PropTypes.string,
   price: PropTypes.number,
@@ -70,7 +89,6 @@ ProductBox.propTypes = {
   favorite: PropTypes.bool,
   compare: PropTypes.bool,
   oldPrice: PropTypes.number,
-
 };
 
 export default ProductBox;
