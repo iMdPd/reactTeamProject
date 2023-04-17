fb53957d5748bfd17d8b81958f73e3db74a188ee
Mateusz Oko <mo89mo@gmail.com>
2023-04-17 10:36:14 +0200

    Add dropdown list

--- src/components/features/ProductSearch/ProductSearch.js
+++ src/components/features/ProductSearch/ProductSearch.js
@@ -10,9 +10,17 @@ const ProductSearch = () => (
   <form action='' className={styles.root}>
     <div className={styles.category}>
       <FontAwesomeIcon className={styles.icon} icon={faListUl} />
-      <select name='' id=''>
-        <option value=''>Select a category</option>
-      </select>
+      <ul>
+        <li>
+          Select a category
+          <ul>
+            <li>New collection</li>
+            <li>Bestsellers</li>
+            <li>Hot deals</li>
+            <li>Coming soon</li>
+          </ul>
+        </li>
+      </ul>
       <FontAwesomeIcon className={styles.icon} icon={faCaretDown} />
     </div>
     <div className={styles.searchField}>
