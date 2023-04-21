c476fac821c28fe644c76418d15518e851c3c7bd
Mateusz Oko <mo89mo@gmail.com>
2023-04-21 23:30:03 +0200

    Change some code, classes and structure

--- src/components/layout/Gallery/Gallery.js
+++ src/components/layout/Gallery/Gallery.js
@@ -7,6 +7,8 @@ import {
   faChevronRight,
   faHeart,
   faExchangeAlt,
+  faEye,
+  faShoppingBasket,
 } from '@fortawesome/free-solid-svg-icons';
 import Button from '../../common/Button/Button';
 
@@ -23,7 +25,28 @@ const Gallery = () => (
             <Button variant='small'>Top rated</Button>
           </div>
           <div className={styles.leftSideMainImage}>
-            <img src='https://images.pexels.com/photos/2082090/pexels-photo-2082090.jpeg'></img>
+            <img
+              src='https://images.pexels.com/photos/2082090/pexels-photo-2082090.jpeg'
+              alt='mainLeft'
+            ></img>
+          </div>
+          <div className={styles.buttons}>
+            <Button variant='icon' className={styles.tooltipButton}>
+              <FontAwesomeIcon icon={faHeart} />
+              <span className={styles.tooltip}>Wishlist</span>
+            </Button>
+            <Button variant='icon' className={styles.tooltipButton}>
+              <FontAwesomeIcon icon={faExchangeAlt} />
+              <span className={styles.tooltip}>Compare</span>
+            </Button>
+            <Button variant='icon' className={styles.tooltipButton}>
+              <FontAwesomeIcon icon={faEye} />
+              <span className={styles.tooltip}>Quick view</span>
+            </Button>
+            <Button variant='icon' className={styles.tooltipButton}>
+              <FontAwesomeIcon icon={faShoppingBasket} />
+              <span className={styles.tooltip}>Add to cart</span>
+            </Button>
           </div>
           <div className={styles.productInfo}>
             <h5>Product name</h5>
@@ -39,32 +62,59 @@ const Gallery = () => (
               <span className={styles.oldPrice}>$160</span>
             </div>
           </div>
-          <div className={styles.buttons}>
-            <Button variant='outline'>Favorite</Button>
-            <Button variant='outline'>Compare</Button>
-            <Button variant='outline'>Look for</Button>
-            <Button variant='outline'>Add to cart</Button>
+          <div className={styles.slider}>
+            <Button className={styles.sliderButton} variant='outline'>
+              <FontAwesomeIcon icon={faChevronLeft} />
+            </Button>
+            <div className={styles.sliderImage}>
+              <div className={styles.thumbnail}>
+                <img
+                  src='https://images.pexels.com/photos/276528/pexels-photo-276528.jpeg'
+                  alt='product1'
+                ></img>
+              </div>
+              <div className={styles.thumbnail}>
+                <img
+                  src='https://images.pexels.com/photos/276534/pexels-photo-276534.jpeg'
+                  alt='product2'
+                ></img>
+              </div>
+              <div className={styles.thumbnail}>
+                <img
+                  src='https://images.pexels.com/photos/11112728/pexels-photo-11112728.jpeg'
+                  alt='product3'
+                ></img>
+              </div>
+              <div className={styles.thumbnail}>
+                <img
+                  src='https://images.pexels.com/photos/3965513/pexels-photo-3965513.jpeg'
+                  alt='product4'
+                ></img>
+              </div>
+              <div className={styles.thumbnail}>
+                <img
+                  src='https://images.pexels.com/photos/4172379/pexels-photo-4172379.jpeg'
+                  alt='product5'
+                ></img>
+              </div>
+              <div className={styles.thumbnail}>
+                <img
+                  src='https://images.pexels.com/photos/11112729/pexels-photo-11112729.jpeg'
+                  alt='product6'
+                ></img>
+              </div>
+            </div>
+            <Button className={styles.sliderButton} variant='outline'>
+              <FontAwesomeIcon icon={faChevronRight} />
+            </Button>
           </div>
-        </div>
-        <div className={styles.slider}>
-          <Button className={styles.sliderButton} variant='outline'>
-            <FontAwesomeIcon icon={faChevronLeft} />
-          </Button>
-          <div className={styles.sliderImage}>
-            <img src='https://images.pexels.com/photos/276528/pexels-photo-276528.jpeg'></img>
-            <img src='https://images.pexels.com/photos/276534/pexels-photo-276534.jpeg'></img>
-            <img src='https://images.pexels.com/photos/11112728/pexels-photo-11112728.jpeg'></img>
-            <img src='https://images.pexels.com/photos/3965513/pexels-photo-3965513.jpeg'></img>
-            <img src='https://images.pexels.com/photos/4172379/pexels-photo-4172379.jpeg'></img>
-            <img src='https://images.pexels.com/photos/11112729/pexels-photo-11112729.jpeg'></img>
-          </div>
-          <Button className={styles.sliderButton} variant='outline'>
-            <FontAwesomeIcon icon={faChevronRight} />
-          </Button>
         </div>
         <div className={styles.rightSide}>
           <div className={styles.rightSideMainImage}>
-            <img src='https://images.pexels.com/photos/7598138/pexels-photo-7598138.jpeg'></img>
+            <img
+              src='https://images.pexels.com/photos/7598138/pexels-photo-7598138.jpeg'
+              alt='mainRight'
+            ></img>
           </div>
           <div className={styles.productInfo}>
             <h5>Product name</h5>
