f36377da374e6794539178d56e0734e12b7ced22
Waldemar Szymon <walldy@tlen.pl>
2023-04-24 16:03:29 +0200

    Fix Carousel component by removing useState hook

--- src/components/common/Carousel/Carousel.js
+++ src/components/common/Carousel/Carousel.js
@@ -1,5 +1,5 @@
 import styles from './Carousel.module.scss';
-import React, { useState } from 'react';
+import React from 'react';
 import { useSwipeable } from 'react-swipeable';
 import PropTypes from 'prop-types';
 
@@ -8,7 +8,7 @@ export const CarouselItem = ({ children }) => {
 };
 
 const Carousel = ({ children, initialIndex, actionSwiped }) => {
-  const [activeIndex, setActiveIndex] = useState(initialIndex);
+  const activeIndex = initialIndex;
 
   const updateIndex = newIndex => {
     if (newIndex < 0) {
@@ -17,7 +17,6 @@ const Carousel = ({ children, initialIndex, actionSwiped }) => {
       newIndex = React.Children.count(children) - 1;
     }
     actionSwiped(newIndex);
-    setActiveIndex(newIndex);
   };
 
   const handlers = useSwipeable({
