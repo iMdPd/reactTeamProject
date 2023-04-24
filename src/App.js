3907fca3bbb6c491b67fb04e8aba65c471c9f57e
Waldemar Szymon <walldy@tlen.pl>
2023-04-24 23:18:34 +0200

    Implementation of multilanguage support

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
@@ -12,17 +12,19 @@ import ProductList from './components/views/ProductList/ProductList';
 import ProductPage from './components/views/ProductPage/ProductPage';
 
 const App = () => (
-  <Provider store={store}>
-    <BrowserRouter>
-      <MainLayout>
-        <Switch>
-          <Route exact path={'/'} component={Homepage} />
-          <Route exact path={'/shop/:categoryId'} component={ProductList} />
-          <Route exact path={'/product/:productId'} component={ProductPage} />
-        </Switch>
-      </MainLayout>
-    </BrowserRouter>
-  </Provider>
+  <Suspense fallback={null}>
+    <Provider store={store}>
+      <BrowserRouter>
+        <MainLayout>
+          <Switch>
+            <Route exact path={'/'} component={Homepage} />
+            <Route exact path={'/shop/:categoryId'} component={ProductList} />
+            <Route exact path={'/product/:productId'} component={ProductPage} />
+          </Switch>
+        </MainLayout>
+      </BrowserRouter>
+    </Provider>
+  </Suspense>
 );
 
 export default App;
---
+++ src/i18n.js
@@ -0,0 +1,19 @@
+import i18n from 'i18next';
+import Backend from 'i18next-http-backend';
+import { initReactI18next } from 'react-i18next';
+
+i18n
+  .use(Backend)
+  .use(initReactI18next)
+
+  .init({
+    lng: 'en', //default language
+    fallbackLng: 'en', //when specified language translations not present
+    debug: false,
+    backend: {
+      //translation file path
+      loadPath: '/assets/{{lng}}/translation.json',
+    },
+  });
+
+export default i18n;
