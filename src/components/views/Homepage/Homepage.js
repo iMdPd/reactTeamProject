25c85781742f824aeb7506a6d2b3b205cf0b3919
Eliza Hynek <eliza.hynek@gmail.com>
2023-04-26 23:01:36 +0200

    change position to resolve conflict

--- src/components/views/Homepage/Homepage.js
+++ src/components/views/Homepage/Homepage.js
@@ -4,8 +4,8 @@ import React from 'react';
 import styles from './Homepage.module.scss';
 
 import FeatureBoxes from '../../features/FeatureBoxes/FeatureBoxes';
-import NewFurniture from '../../features/NewFurniture/NewFurnitureContainer';
 import Promoted from '../../features/Promoted/Promoted';
+import NewFurniture from '../../features/NewFurniture/NewFurnitureContainer';
 
 const Homepage = () => (
   <div className={styles.root}>
