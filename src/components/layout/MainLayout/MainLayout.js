277b8e31e00730db981e512d258bd3275609c2d9
Mariusz Politowicz <politowiczmar@gmail.com>
2023-04-27 00:23:45 +0200

    Add logic to NewsletterModule

--- src/components/layout/MainLayout/MainLayout.js
+++ src/components/layout/MainLayout/MainLayout.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import Header from '../Header/Header';
@@ -6,23 +6,55 @@ import Footer from '../Footer/Footer';
 import { StickyBar } from '../StickyBar/StickyBar';
 import Feedbacks from '../../features/Feedbacks/Feedbacks';
 import { useLocation } from 'react-router-dom';
+import { NewsletterModal } from '../../features/NewsletterModal/NewsletterModal';
+import { useSelector } from 'react-redux';
+import { getNewsleterByEmail } from '../../../redux/newsletterRedux';
 
-const MainLayout = ({ children }) => {
+const timeout = 15000;
+
+const MainLayout = ({ children, user }) => {
+  const [modalShow, setModalShow] = useState(false);
+  const [count, setCount] = useState(0);
+
   const { pathname } = useLocation();
 
+  const userSignedToNewsletter = useSelector(state =>
+    getNewsleterByEmail(state, user.email)
+  );
+
+  const incorrectPath = pathname !== '/signup' && pathname !== '/login';
+
+  if (count < 1 && incorrectPath && !userSignedToNewsletter) {
+    setTimeout(() => setModalShow(true), timeout);
+    setCount(prev => prev + 1);
+  }
+
+  const handleMouseLeave = () => {
+    if (count <= 1 && incorrectPath && !userSignedToNewsletter) {
+      setModalShow(true);
+      setCount(prev => prev + 1);
+    }
+  };
+
   return (
-    <div>
+    <div
+      onMouseLeave={handleMouseLeave}
+      style={{ filter: modalShow ? 'blur(20px)' : 'none' }}
+    >
       <Header pathname={pathname} />
       {children}
-      {pathname !== '/signup' && pathname !== '/login' && <StickyBar />}
-      {pathname !== '/signup' && pathname !== '/login' && <Feedbacks />}
+      {incorrectPath && ((<StickyBar />), (<Feedbacks />))}
       <Footer pathname={pathname} />
+      {incorrectPath && (
+        <NewsletterModal show={modalShow} onHide={() => setModalShow(false)} />
+      )}
     </div>
   );
 };
 
 MainLayout.propTypes = {
   children: PropTypes.node,
+  user: PropTypes.shape({ email: PropTypes.string, password: PropTypes.string }),
 };
 
 export default MainLayout;
